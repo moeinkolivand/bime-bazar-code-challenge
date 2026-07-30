de20547279dcb482191ac6eeaa7d1299f27c835a
moein <moeinkolivand97@gmail.com>
2026-07-30 18:24:23 +0330

    refactor: add imports at init file for better importing

---
+++ app/core/database/__init__.py
@@ -0,0 +1,2 @@
+from app.core.database.postgres.postgres import get_db_postgres
+from app.core.database.redis.redis_repository import get_redis_repository, RedisRepository
---
+++ app/modules/user/__init__.py
@@ -0,0 +1,2 @@
+from app.modules.user.routes.auth_routes import router as user_router
+from app.modules.user.exceptions.exception_handlers import register_user_exception_handlers
