5323377fb9004f13b832c301ab437bde591a9389
moein <moeinkolivand97@gmail.com>
2026-07-30 18:30:39 +0330

    feat: fix main and creating app and registring routes and exceptions

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+from fastapi import APIRouter, FastAPI
+from app.modules.user import user_router, register_user_exception_handlers
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+def create_app() -> FastAPI:
+    app = FastAPI(title="BimeBazar API", version="1.0.0")
+    api_v1_router = APIRouter(prefix="/api/v1")
+
+    api_v1_router.include_router(user_router)
+    app.include_router(api_v1_router)
+    return app
+
+
+app = create_app()
+register_user_exception_handlers(app)
+
+
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
